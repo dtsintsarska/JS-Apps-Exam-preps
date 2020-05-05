270ce7e914f283348b136d6184cd3b76e5aa1410
dtsintsarska <64830409+dtsintsarska@users.noreply.github.com>
2020-05-05 13:02:36 +0300

    Update firebase.js

--- Exam/config/firebase.js
+++ Exam/config/firebase.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyA-e6knwqKg8e6r4_0z_PN1YB2YG7Ao9Qg",
-    authDomain: "exam-30768.firebaseapp.com",
-    databaseURL: "https://exam-30768.firebaseio.com",
-    projectId: "exam-30768",
-    storageBucket: "exam-30768.appspot.com",
-    messagingSenderId: "534722425550",
-    appId: "1:534722425550:web:6d733daf66fa664732969f"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
